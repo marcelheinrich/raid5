235ecb18e6732329b8e368d8da4a6c4ed61c0190
Marcel <marcel.henrich@ibiruba.ifrs.edu.br>
2019-08-08 18:07:48 -0300

    alteração

---
+++ recebedados.php
@@ -0,0 +1,12 @@
+<?php
+
+    $palavra = $_POST['palavra'];
+    $contPalavra = count($palavra);
+    $tamanhoPalavra = (ceil($contPalavra / 2)) * 2;
+
+    if($contPalavra == ($tamanhoPalavra){
+        
+    }
+
+
+?>
